c6b95b58225ecb406c805e154372a6bdbc33a37f
dykim <onurleft@naver.com>
2024-08-14 14:43:29 +0900

    ts study 1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type Name = string | number
+const name : Name[] = ['a']
+console.log(name)
+
+function a (x:number) :number {
+  return x
+}
+a(1)
+
+type Member = [number, boolean]
+const john :Member = [123, true]
+
+class User {
+  name :string;
+  constructor (name :string) {
+    this.name = name
+
+  }
+}
+
+const myInfo :{
+  name :string[], 
+  age:number, 
+  home :string
+} = {
+  name : ['kim'],
+  age : 29,
+  home : '서울',
+}
+console.log(myInfo)
+
+const aaa :(number | string)[] = [1,'2',3]
+
+function bbb (x :number|string) { // return이 없을 경우 :void (return한 경우 에러)
+  const arr :number[] = []
+  if (typeof x === 'string') {
+    return x + '1'
+  } else {
+    return x + 1
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
